bc632647d9de20c8be0eaf05105cc82b125669db
elv-wayne <wayne.tran@eluv.io>
2020-04-17 00:18:05 -0400

    Add fabric and load site on request

--- elv-tvos/start.js
+++ elv-tvos/start.js
@@ -9,7 +9,8 @@ var {JQ,isEmpty} = require('./server/utils')
 const main = async () => {
   let configUrl = Config.configUrl;
   let siteId = Config.siteId;
-  let server_port = Config.server_port || 4001;
+  let serverHost = Config.serverHost;
+  let serverPort = Config.serverPort || 4001;
   let privateKey = process.env.PRIVATEKEY;
 
   if(isEmpty(configUrl)){
@@ -27,31 +28,33 @@ const main = async () => {
     process.exit(1);
   }
 
-  console.log("Initializing Fabric.");
-  let fabric = new Fabric;
-  await fabric.init({configUrl,privateKey});
-  let site = new Site({fabric, siteId});
-  await site.loadSite();
-
   var app = express();
 
   app.engine('hbs', exbars({defaultLayout: false}));
   app.set('view engine', 'hbs');
 
-  const params = {
-    title_logo: site.siteInfo.title_logo,
-    display_title: site.siteInfo.display_title,
-    playlists: site.siteInfo.playlists
-  };
 
-  app.get('/index.hbs', function(req, res) {
+
+  app.get('/index.hbs', async function(req, res) {
+
+    let fabric = new Fabric;
+    await fabric.init({configUrl,privateKey});
+    let site = new Site({fabric, siteId});
+    await site.loadSite();
+
+    const params = {
+      title_logo: site.siteInfo.title_logo,
+      display_title: site.siteInfo.display_title,
+      playlists: site.siteInfo.playlists,
+      eluvio_logo: serverHost + ":" + serverPort + "/logo.png"
+    };
     res.render('index', params);
   });
   
   app.use(express.static('static'));
-  console.log("Server running on port: " + server_port);
+  console.log("Server running on port: " + serverPort);
   
-  app.listen(server_port);
+  app.listen(serverPort);
 }
 
 
